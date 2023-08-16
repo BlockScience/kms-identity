51b1182a88b64176f8508733933e2567a4beca21
Aeolus1364 <millerluke1364@gmail.com>
2023-08-15 17:12:55 -0700

    testing governance graph construction, trying out new internal api testing (vscode rest client)

---
+++ tests/gov_test.py
@@ -0,0 +1,50 @@
+import api
+
+api.database.drop()
+
+luke = api.objects.create_object(obj={
+    "rid": "user:luke"
+})["rid"]
+
+orion = api.objects.create_object(obj={
+    "rid": "user:orion"
+})["rid"]
+
+func = api.objects.create_object(obj={
+    "rid": "func:gov1"
+})["rid"]
+
+action1 = api.objects.create_object(obj={
+    "rid": "action:1"
+})["rid"]
+
+action2 = api.objects.create_object(obj={
+    "rid": "action:2"
+})["rid"]
+
+working_doc = api.objects.create_object(obj={
+    "transform": {
+        "reference": "https://hackmd.io/TBxOcWn_SpWEEcx_t_9GWw?view",
+        "from": "url",
+        "to": "hackmd"
+    }
+})["rid"]
+
+assertion = api.assertions.create_undirected_assertion(obj={
+    "name": "Working Doc",
+    "description": "Points to current working doc for identity system development",
+    "members": [working_doc]
+})["rid"]
+
+func = api.assertions.create_directed_assertion(obj={
+    "name": "Gov Func",
+    "from": [func],
+    "to": [action1, action2]
+})["rid"]
+
+gov = api.assertions.create_directed_assertion(obj={
+    "name": "Working Doc Governance",
+    "definition": func,
+    "from": [luke, orion],
+    "to": [assertion]
+})
